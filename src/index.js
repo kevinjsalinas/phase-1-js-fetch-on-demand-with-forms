6cd3069bfe5ca3f6b3c5d2618838e5fd0e7d70d3
Kevin Salinas <kevinsalinas155@gmail.com>
2023-02-05 22:18:59 -0500

    completed fetch on demand w forms lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-const init = () => {
+const init = () => {   //this is a function but inside a variable 
+
+    const form = document.querySelector("form"); 
+
+    form.addEventListener("submit", (e) => {    
+    e.preventDefault();
+    const input = document.querySelector("#searchByID");
+    
+    //console.log(input.value)
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+        .then ((response) => response.json())
+        .then ((data) => {  
+            const h4 = document.querySelector("h4");
+            const p = document.querySelector("p");
+
+            h4.innerText = data.title;
+            p.textContent = data.summary;
+        })
   
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
